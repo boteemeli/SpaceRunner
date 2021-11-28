c47b837892e3f5101171cb96c8abe759ac479e38
boteemeli <eemeli.gustafsson@hotmail.com>
2021-11-29 00:26:01 +0200

    pausemenu&quitbutton added

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GamePaused = false;
+    public GameObject ui;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePaused == true)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+
+    }
+    public void Resume()
+    {
+        ui.SetActive(false);
+        Time.timeScale = 1;
+        GamePaused = false;
+    }
+    public void Pause()
+    {
+        ui.SetActive(true);
+        Time.timeScale = 0;
+        GamePaused = true;
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
