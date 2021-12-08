55dbe93ff8e3716e290d6179693eb42c69dbdde8
Sanni Sjöholm <sjoholm.sanni@gmail.com>
2021-12-08 13:10:21 +0200

    viime hetken muutoksia
    last changes

--- Assets/Scripts/EndMenu.cs
+++ Assets/Scripts/EndMenu.cs
@@ -7,7 +7,7 @@ public class EndMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3);
     }
     public void ToTheMenu()
     {
